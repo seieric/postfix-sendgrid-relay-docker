bca2cc5e2f7f1b1961926b8e78948a0dde63a74f
seieric <30688855+seieric@users.noreply.github.com>
2022-08-20 01:04:05 +0900

    init

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM ubuntu:22.04
+
+RUN echo "postfix postfix/mailname string localhost" && echo "postfix postfix/main_mailer_type string 'Local only'" | debconf-set-selections && \
+    apt-get update && apt-get install postfix libsasl2-modules && \
+    apt-get clean && rm -rf /var/lib/apt/lists/* && \
+    cp /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
+
+COPY configs/main.cf /etc/postfix/
+COPY configs/master.cf /etc/postfix/
+COPY init-postfix.sh /
+
+EXPOSE 25
+EXPOSE 587
+
+ENTRYPOINT ["/init-postfix.sh"]
---
+++ init-postfix.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if [[ -z $SENDGRID_API_KEY ]]; then
+  echo "You need to set SENDGRID_API_KEY to the container." >&2
+  exit -1
+fi
+
+echo "[smtp.sendgrid.net]:587 apikey:$SENDGRID_API_KEY" >> /etc/postfix/sasl_passwd
+
+if [[ -n $SMTP_USER ]] && [[ -n $SMTP_PASSWORD ]]; then
+  echo "[localhost.localdomain]:587 $SMTP_USER:$SMTP_PASSWORD" >> /etc/postfix/sasl_passwd
+fi
+
+/usr/sbin/postmap /etc/postfix/sasl_passwd && rm /etc/postfix/sasl_passwd && chmod 600 /etc/postfix/sasl_passwd.db && \
+/usr/sbin/postfix start
